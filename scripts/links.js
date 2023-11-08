2e90fa8ded87b21fca11c2171f274672903d5e9a
taren-moss <tarendmoss@gmail.com>
2023-11-08 15:26:38 -0800

    Home Update

---
+++ scripts/links.js
@@ -0,0 +1,30 @@
+const list = document.querySelector("ul");
+
+const baseURL = "https://taren-moss.github.io/wdd230/";
+const linksURL = "https://taren-moss.github.io/wdd230/data/links.json";
+
+async function GetLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    DisplayLinks(data.lessons);
+}
+
+function DisplayLinks(weeks) {
+    weeks.forEach((week) => {
+        let item = document.createElement('li');
+        item.textContent = `Week ${week.lesson}: `;
+
+        week.links.forEach((link) => {
+            let newLink = document.createElement('a');
+            newLink.setAttribute('href', link.url);
+            newLink.setAttribute('target', "_blank");
+            newLink.textContent = link.title;
+            item.appendChild(newLink);
+            item.append(' | ');
+        })
+
+        list.appendChild(item);
+    })
+}
+  
+  GetLinks();
