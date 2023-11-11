715b62b79bde5f88a95b8d83584604d93969b379
taren-moss <tarendmoss@gmail.com>
2023-11-11 15:10:21 -0800

    Fixes

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,6 +1,8 @@
 const space = document.querySelector('.directory');
 const gridButton = document.querySelector('#grid-button');
 const listButton = document.querySelector('#list-button');
+const themeSwitch = document.querySelector('.switch');
+const themeValue = document.querySelector('#theme-switch');
 
 async function GetCompanyData(DisplayType) {
     const response = await fetch("data/members.json");
@@ -71,6 +73,47 @@ function DisplayList(data) {
     space.appendChild(list);
 }
 
+function ThemeToggle() {
+    const card = document.querySelectorAll('.card');
+    const label = document.querySelectorAll('.card > h3');
+    var root = document.querySelector(`:root`);
+
+    if (themeValue.checked) {
+        root.style.setProperty('--main-color', '#ffc86a');
+        root.style.setProperty('--secondary-color', '#f4a321');
+        root.style.setProperty('--accent-color', '#683571');
+        root.style.setProperty('--text-color', '#38175a');
+        root.style.setProperty('--hover-color', '#683571');
+        root.style.setProperty('--background-color', '#ffe78e');
+        root.style.setProperty('--slider-color', '#683571');
+        root.style.setProperty('--slider-background', '#38175a');
+        root.style.setProperty('--shadow-color', 'rgb(244, 163, 33, 0.75)');
+
+        card.forEach((i) => (i.style.setProperty('background-color', 'var(--main-color)')));
+        card.forEach((i) => (i.style.setProperty('border-color', 'var(--secondary-color)')));
+        card.forEach((i) => (i.style.setProperty('border-bottom-color', 'var(--accent-color)')));
+        label.forEach((i) => (i.style.setProperty('background-color', 'var(--secondary-color)')));
+    }
+    else {
+        root.style.setProperty('--main-color', '#683571');
+        root.style.setProperty('--secondary-color', '#512466');
+        root.style.setProperty('--accent-color', '#f4a321');
+        root.style.setProperty('--text-color', '#ffe78e');
+        root.style.setProperty('--hover-color', '#ffc86a');
+        root.style.setProperty('--background-color', '#38175a');
+        root.style.setProperty('--slider-color', '#ffe78e');
+        root.style.setProperty('--slider-background', '#f4a321');
+        root.style.setProperty('--shadow-color', 'rgb(56, 23, 90, 0.75)');
+            
+        card.forEach((i) => (i.style.setProperty('background-color', 'var(--secondary-color)')));
+        card.forEach((i) => (i.style.setProperty('border-color', 'var(--main-color)')));
+        card.forEach((i) => (i.style.setProperty('border-bottom-color', 'var(--accent-color)')));
+        label.forEach((i) => (i.style.setProperty('background-color', 'var(--main-color)')));
+    }
+}
+
+themeSwitch.addEventListener('click', () => ThemeToggle())
+
 gridButton.setAttribute('class', 'active');
 listButton.setAttribute('class', '');
 space.innerHTML = "";
@@ -81,7 +124,6 @@ gridButton.addEventListener('click', () => {
     listButton.setAttribute('class', '');
     space.innerHTML = "";
     GetCompanyData(DisplayGrid)
-
 });
 
 listButton.addEventListener('click', () => {
